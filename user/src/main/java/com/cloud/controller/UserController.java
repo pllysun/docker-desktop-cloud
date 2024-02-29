a1a39b00ee9abeb91174b2d48d6fd371a8cf3ccf
阿东i小叶子 <1125887000@qq.com>
2024-02-29 21:02:04 +0800

    :sparkles:增加用户信息相关接口

--- user/src/main/java/com/cloud/controller/UserController.java
+++ user/src/main/java/com/cloud/controller/UserController.java
@@ -72,7 +72,10 @@ public class UserController {
         return R.success("注册成功，请登录");
     }
 
-
+    /**
+     * 获取用户信息(单个用户信息)
+     * @return 用户信息
+     */
     @GetMapping("/userInfo")
     public R<Object> getUserInfo(HttpServletRequest request){
         String username = JwtUtil.getUsername(request);
@@ -82,6 +85,74 @@ public class UserController {
         return  R.success(one);
     }
 
+    /**
+     * 获取用户列表
+     * @return 用户列表
+     */
+    @GetMapping("/userList")
+    public R<Object> getUserList(){
+        List<Users> list = usersService.list();
+        return R.success(list);
+    }
+
+
+    /**
+     * 删除用户
+     * @param user 用户名
+     * @return 删除结果
+     */
+    @PostMapping("/deleteUser")
+    public R<Object> deleteUser(@RequestBody UserDTO user){
+        LambdaQueryWrapper<Users> lambda = new QueryWrapper<Users>().lambda();
+        lambda.eq(Users::getUsername, user.getUsername());
+        boolean remove = usersService.remove(lambda);
+        if(remove){
+            return R.success("删除成功");
+        }
+        return R.fail("删除失败");
+    }
+
+    /**
+     * 修改用户信息
+     * @param user 用户名和密码
+     * @return 修改结果
+     */
+    @PostMapping("/updateUser")
+    public R<Object> updateUser(@RequestBody UserDTO user){
+        LambdaQueryWrapper<Users> lambda = new QueryWrapper<Users>().lambda();
+        lambda.eq(Users::getUsername, user.getUsername());
+        Users users = usersService.getOne(lambda);
+        if(users!=null){
+            users.setPassword(user.getPassword());
+            boolean update = usersService.updateById(users);
+            if(update){
+                return R.success("修改成功");
+            }
+            return R.fail("修改失败");
+        }
+        return R.fail("用户不存在");
+    }
+
+    /**
+     * 退出登录
+     * @return 退出结果
+     */
+    @GetMapping("/logout")
+    public R<Object> logout(HttpServletRequest request){
+        String username = JwtUtil.getUsername(request);
+        redisCache.deleteObject("User:"+username);
+        return R.success("退出成功");
+    }
+
+    /**
+     * 获取用户数量
+     * @return 用户数量
+     */
+    @GetMapping("/userCount")
+    public R<Object> userCount(){
+        int count = (int) usersService.count();
+        return R.success(count);
+    }
 
 
 }
