833c6bf185993b12896d3d996b9cff53ba1d0002
adong <1125887000@qq.com>
2024-03-08 15:13:39 +0800

    :sparkles:新增docker运行环境

--- user/src/main/java/com/cloud/controller/UserController.java
+++ user/src/main/java/com/cloud/controller/UserController.java
@@ -10,10 +10,7 @@ import com.cloud.utils.R;
 import com.cloud.utils.RedisCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -26,6 +23,7 @@ import java.util.List;
  **/
 @RestController
 @Controller
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UsersService usersService;
